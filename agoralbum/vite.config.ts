2e13741f740cb1eb22827a4549087bb8001b68ea
demilolmanso <118947961+demilolmanso@users.noreply.github.com>
2026-06-18 01:17:55 -0300

    Update vite.config.ts

--- agoralbum/vite.config.ts
+++ agoralbum/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/agoralbum/', // 👈 ESTO ES CLAVE: Le avisa a Vite que va en agorlab.com/agoralbum
+  base: '/agoralbum/', // 👈 ESTO ES CRUCIAL para que no quede la pantalla en blanco
 })
